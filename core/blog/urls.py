2dc4cb06292de86ff877d65dd1ebbe9aab085732
Elia Deppe <elia.deppe7@gmail.com>
2022-12-18 18:06:55 -0500

    Update: post_edit_button.html is now post_options.html. Urls and Views updated to use List View for index.

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('post/<int:pk>', views.detail, name='detail'),
     path('post/new', views.new_post, name='new_post'),
     path('post/<int:pk>/edit', views.edit_post, name='edit_post')
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
+from django.views import generic
 
 from .forms import PostForm
 from .models import Post
 
 
-def index(request):
-    posts = Post.objects.filter(date_published__isnull=False).order_by('-date_published')
-    context = {
-        'posts': posts
-    }
-    return render(request, 'blog/index.html', context)
+class IndexView(generic.ListView):
+    model = Post
+    ordering = ['-id']
+
+    context_object_name = 'posts'
+    template_name = 'blog/index.html'
+
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
