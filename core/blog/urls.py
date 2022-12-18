0f0af45afc5c07bf6abe8957980e8e4ade118e7c
Elia Deppe <elia.deppe7@gmail.com>
2022-12-17 21:37:12 -0500

    Update blog/urls.py to give a detail post at .../post/<int:pk>

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:pk>', views.detail, name='detail')
+    path('post/<int:pk>', views.detail, name='detail')
 ]
