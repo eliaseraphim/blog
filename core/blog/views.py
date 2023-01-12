efd28dae7fd0c4e2255b478508cdccd137325576
Elia Deppe <elia.deppe7@gmail.com>
2023-01-12 01:20:29 -0500

    Updated documentation in blog.views.

--- core/blog/views.py
+++ core/blog/views.py
@@ -8,12 +8,27 @@ from .models import Post
 
 
 class IndexView(generic.ListView):
+    """
+    .. py:class:: IndexView
+        The view class for the index page of the blog. Inherits from :py:class:`django.views.generic.ListView`.
+
+        :cvar :py:class:`django.db.Model` model: The model for the view. Set to: :py:class:`blog.models.Post`
+        :cvar :py:class:`str` context_object_name: The name of the object returned by :py:meth:`get_context_data(self, **kwargs)`. Set to: ``'posts'``.
+        :cvar :py:class:`str` template_name: The name of the template for the view. Set to: ``'blog/index.html'``.
+    """
+
     model = Post
 
     context_object_name = 'posts'
     template_name = 'blog/index.html'
 
     def get_queryset(self):
+        """
+        Return a :py:class:`QuerySet` of posts.
+
+        :returns: A set of Posts ordered by :py:attr:`date_published` (descending).
+        :rtype: :py:class:`QuerySet`
+        """
         return Post.objects.filter(date_published__isnull=False).order_by('-date_published')
 
 
@@ -43,6 +58,13 @@ class EditPostView(generic.UpdateView):
     template_name = 'blog/post/actions/edit.html'
 
     def get_context_data(self, **kwargs):
+        """
+        Insert the form into the context dict, with additional context for action button.
+
+        :returns: ``context_data``
+        :rtype: ``dict``
+        """
+
         context_data = super().get_context_data(**kwargs)
         context_data.update({
             'form_action': 'Edit Post',
@@ -56,6 +78,9 @@ class EditPostView(generic.UpdateView):
 
 
 class DeletePostView(generic.DeleteView):
+    """
+
+    """
     model = Post
 
     context_object_name = 'post'
