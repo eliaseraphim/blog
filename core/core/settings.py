5eaf960a18e8f0fd1efd82bee845ad5bcfa057ad
Elia Deppe <elia.deppe7@gmail.com>
2023-01-13 01:32:39 -0500

    Created account urls, and modified the navbar.

--- core/core/settings.py
+++ core/core/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,3 +127,6 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect Information
+LOGIN_REDIRECT_URL = '/'
