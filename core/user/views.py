462d4d0f1a88cd9fbb9a2fe79e06715fb4c649cf
Elia Deppe <elia.deppe7@gmail.com>
2023-01-21 16:04:21 -0500

    Updated user page.

--- core/user/views.py
+++ core/user/views.py
@@ -1,9 +1,13 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView, TemplateView
 
 
 _User = get_user_model()
 
 
-class UserView(TemplateView):
+class UserView(LoginRequiredMixin, TemplateView):
     template_name = 'user/user.html'
+
+class UserLoginView():
+    pass
