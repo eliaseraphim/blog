bc6bb0852c3bdabc6a420dcb6a1ec972226ed426
Elia Deppe <elia.deppe7@gmail.com>
2023-01-21 14:26:18 -0500

    Created avatar, bio, and website fields for user.

--- core/user/models.py
+++ core/user/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -7,12 +8,29 @@ from .managers import UserManager
 
 # Create your models here.
 class User(AbstractUser):
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(
+        _("username"),
+        max_length=150,
+        unique=True,
+        help_text=_(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        validators=[username_validator],
+        error_messages={
+            "unique": _("A user with that username already exists."),
+        },
+    )
     email = models.EmailField(
         _('email address'),
         max_length=64,
         blank=False,
         help_text=_('Required. Please enter a valid email.'),
     )
+    avatar = models.ImageField(blank=True, null=True, upload_to=f'users/{username}/%Y/%m/%d')
+    bio = models.CharField(null=True, blank=True, max_length=255)
+    website = models.CharField(null=True, blank=True, max_length=255)
 
     objects = UserManager()
 
